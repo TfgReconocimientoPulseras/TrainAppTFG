2bac62e43cb5a4d48bbf77dd29cee7f6d8de3ea5
11crom11 <d_danigarcia@hotmail.es>
2017-05-09 15:04:33 +0200

    - Colocado calendario en el fragment de historial. - Creada activity que se encargará de mostrar la lista de actividades realizadas.

--- app/src/main/java/com/example/ivana/trainapptfg/Fragments/HistorialFragment.java
+++ app/src/main/java/com/example/ivana/trainapptfg/Fragments/HistorialFragment.java
@@ -1,16 +1,23 @@
 package com.example.ivana.trainapptfg.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
 
+import com.example.ivana.trainapptfg.Historial.HistorialDiaConcreto;
 import com.example.ivana.trainapptfg.R;
 
 
 public class HistorialFragment extends Fragment {
+
+    private CalendarView calendario;
 
 
     public HistorialFragment() {
@@ -21,8 +28,27 @@ public class HistorialFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_historial, container, false);
+        View view = inflater.inflate(R.layout.fragment_historial, container, false);
+
+        this.calendario = (CalendarView) view.findViewById(R.id.calendario_historial);
+        configuracionCalendario();
+
+        return view;
+    }
+
+    private void configuracionCalendario(){
+        this.calendario.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Intent intent = new Intent(getContext(), HistorialDiaConcreto.class);
+                intent.putExtra("year", year);
+                intent.putExtra("month", month);
+                intent.putExtra("dayOfMonth", dayOfMonth);
+                startActivity(intent);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/ivana/trainapptfg/Historial/HistorialDiaConcreto.java
@@ -0,0 +1,26 @@
+package com.example.ivana.trainapptfg.Historial;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.ivana.trainapptfg.R;
+
+public class HistorialDiaConcreto extends Activity {
+
+    private TextView textoPruebas;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial_dia_concreto);
+        this.textoPruebas = (TextView)findViewById(R.id.textoPrueba);
+        Intent i = getIntent();
+        this.textoPruebas.setText("Aquí aparecerá una lista con las actividades que has realizado en el día " + Integer.toString(i.getIntExtra("dayOfMonth", 1)) + " de " + Integer.toString(i.getIntExtra("month", 1)) + " del " + Integer.toString(i.getIntExtra("year", 1970)) + " ;)");
+    }
+
+    protected void onResume() {
+
+        super.onResume();
+    }
+}
